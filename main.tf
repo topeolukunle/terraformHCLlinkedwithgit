ab953fb727e3cde2e60393b87d4e75b6f04fedd0
Daniel <Topeolukunle2@gmail.com>
2026-09-04 10:06:31 -0500

    feat: reference web-server module from HCP Private Registry

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
+# Reference the private registry module
+module "web_server" {
+  source        = "app.terraform.io/YOUR-ORG/web-server/aws"
+  version       = "1.0.0"
+  instance_type = "t2.micro"
+  ami_id        = "ami-0c02fb55956c7d316"
+}
+
 output "hello" {
   value = "HCP Terraform remote backend is working"
 }# updated
 # retry
+
+output "lab_version" {
+  value = "lab6-hcp-terraform"
+}
