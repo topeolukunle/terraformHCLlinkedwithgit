adbd89e69ed2895648975e80534c88c3eeec01d5
Daniel <Topeolukunle2@gmail.com>
2026-09-04 10:13:12 -0500

    fix: replace placeholder with real org name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Reference the private registry module
 module "web_server" {
-  source        = "app.terraform.io/YOUR-ORG/web-server/aws"
+  source        = "app.terraform.io/ikini/web-server/aws"
   version       = "1.0.0"
   instance_type = "t2.micro"
   ami_id        = "ami-0c02fb55956c7d316"
